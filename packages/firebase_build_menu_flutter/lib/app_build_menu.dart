98dbbfb96e57b6794c0c9e3d9cd58c7be69c5d26
Alexandre Roux <alex@tekartik.com>
2024-03-13 09:16:44 +0100

    export test menu from build_menu_flutter

--- packages/firebase_build_menu_flutter/lib/app_build_menu.dart
+++ packages/firebase_build_menu_flutter/lib/app_build_menu.dart
@@ -1,6 +1,21 @@
 export 'package:tekartik_firebase_build/app_build.dart';
 export 'package:tekartik_firebase_build/firebase_deploy.dart';
-export 'package:tekartik_test_menu/test_menu.dart';
+export 'package:tekartik_test_menu/test_menu.dart'
+    show
+        menu,
+        enter,
+        item,
+        write,
+        leave,
+        prompt,
+        showMenu,
+        command,
+        // ignore: deprecated_member_use
+        solo_item,
+        // ignore: deprecated_member_use
+        solo_menu,
+        // ignore: deprecated_member_use
+        devWrite;
 export 'package:tekartik_test_menu_io/test_menu_io.dart'
     show mainMenu, initTestMenuConsole;
 
